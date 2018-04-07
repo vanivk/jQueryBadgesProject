f6d8fbc7cc5dc58fa4cd3f0b2b2c085a5e71bad7
Vani Kulkarni <vani.kulkarni@mesitis.com>
2018-04-07 16:54:47 +0530

    Commit: Changes to jquerybadgesproject

--- assets/main.js
+++ assets/main.js
@@ -1,5 +1,38 @@
-$(function() {
+$(function () {
 
-  // your code will go here
+  $.ajax({
+    url: 'https://www.codeschool.com/users/vanivkulkarni.json',
+    dataType: 'jsonp',
+    success: function (response) {
+      // handle response
+      addCourses(response.courses.completed)
+    }
+  });
+
+  function addCourses(courses) {
+    var $badges = $('badges');
+    courses.forEach(function (course) {
+      var $course = $('<div />', {
+        'class': 'course'
+      }).appendTo($badges);
+
+      $('<h3 />', {
+        text: course.title
+      }).appendTo($course);
+
+      $('<img />', {
+        src: source.badge
+      }).appendTo($course);
+
+      $('<a />', {
+        'class': 'btn btn-primary',
+        target: '_blank',
+        href: course.url,
+        text: 'See Course',
+
+      }).appendTo($course);
+
+    });
+  }
 
 });
